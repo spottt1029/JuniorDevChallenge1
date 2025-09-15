729ed851ffc12d8b30705e7515b9d8c3718e3afc
0130 <0130@D900TA>
2025-09-15 17:20:15 +0800

    新增大小寫都可使用，有限制要用n才能停止，輸入其他的會報錯

--- Program.cs
+++ Program.cs
@@ -7,27 +7,44 @@
             // 建立兩個 List 來儲存學生姓名與成績
             List<string> names = new List<string>();
             List<int> grades = new List<int>();
-            string again = "Y";
+            string again = "y";
 
             // 使用 while 迴圈允許使用者不斷輸入學生資料
-            while (again == "Y")
+            while (again.ToLower() == "y")
             {
                 Console.WriteLine("請輸入學生姓名: ");
-                string n = Console.ReadLine(); // 讀取學生姓名
-                names.Add(n);
+                string stu_name = Console.ReadLine(); // 讀取學生姓名
+                names.Add(stu_name);
 
                 Console.WriteLine("請輸入學生成績: ");
-                int g = Convert.ToInt32(Console.ReadLine());
-                grades.Add(g);
+                int stu_grade = Convert.ToInt32(Console.ReadLine());
+                grades.Add(stu_grade);
 
-                Console.WriteLine("是否要輸入另一位學生？(Y/N)");
-                again = Console.ReadLine();
+                while (true)
+                {
+                    Console.WriteLine("是否要輸入另一位學生？(Y/N)");
+                    again = Console.ReadLine().Trim().ToLower();
+
+                    if (again == "y" || again == "n")
+                    {
+                        break; // 正確回答才跳出這個小迴圈
+                    }
+                    else
+                    {
+                        Console.WriteLine("請回答 y 或 n !");
+                    }
+                }
             }
 
             // 根據學生成績輸出對應的等級
             for (int i = 0; i < names.Count; i++)
             {
-                if (grades[i] >= 90)
+
+                if (grades[i] > 100)
+                {
+                    Console.WriteLine("輸錯成績了");
+                }
+                else if (grades[i] >= 90)
                 {
                     Console.WriteLine(names[i] + " 的成績是 A");
                 }
